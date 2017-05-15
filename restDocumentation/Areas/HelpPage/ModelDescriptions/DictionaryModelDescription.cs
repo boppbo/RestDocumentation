2602bbdffc8854567bc5f3ffffa4bf10b17e92fc
Boris Bopp <boris.bopp@gmail.com>
2017-05-15 15:44:18 +0200

    Add project files

---
+++ restDocumentation/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace restDocumentation.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ restDocumentation/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace restDocumentation.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
